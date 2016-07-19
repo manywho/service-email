343a5f1c081dc57b57b43c15db4ccadd1a4e26a0
jose <jose.collazzi@manywho.com>
2016-07-19 11:40:18 +0100

    CORE-2296added test for syncronous mails

--- src/test/java/com/manywho/services/email/controllers/SendEmailControllerTest.java
+++ src/test/java/com/manywho/services/email/controllers/SendEmailControllerTest.java
@@ -2,15 +2,16 @@ package com.manywho.services.email.controllers;
 
 import com.manywho.sdk.utils.AuthorizationUtils;
 import com.manywho.services.email.test.EmailServiceFunctionalTest;
-import junit.framework.Assert;
 import org.codemonkey.simplejavamail.Email;
 import org.codemonkey.simplejavamail.Mailer;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+
+import javax.mail.Session;
 import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
-import javax.mail.Session;
+
 import static java.lang.Thread.sleep;
 import static junit.framework.TestCase.assertEquals;
 import static org.mockito.Mockito.*;
@@ -123,5 +124,93 @@ public class SendEmailControllerTest extends EmailServiceFunctionalTest {
     // todo we should find a way to wait until the thread finish without use sleep
     private void sleepUntilThreadFinish() throws InterruptedException {
         sleep(1000);
+    }
+
+    @Test
+    public void testSendEmailSimple() throws Exception {
+        MultivaluedMap<String,Object> headers = new MultivaluedHashMap<>();
+        headers.add("Authorization", AuthorizationUtils.serialize(getDefaultAuthenticatedWho()));
+
+        when(emailServiceMock.createEmailSimple(any(), any())).thenCallRealMethod();
+
+        Response responseMsg = target("/actions/email-simple").request()
+                .headers(headers)
+                .post(getServerRequestFromFile("SendEmailController/simple/request-send-simple-email.json"));
+
+        //check the response is right
+        assertJsonSame(
+                getJsonFormatFileContent("SendEmailController/simple/response-send-simple-email.json"),
+                getJsonFormatResponse(responseMsg)
+        );
+
+        ArgumentCaptor<Email> argumentCaptorEmail = ArgumentCaptor.forClass(Email.class);
+        ArgumentCaptor<Mailer> argumentCaptorMailer = ArgumentCaptor.forClass(Mailer.class);
+
+        // before verify the call to send email, we need to give some time to the threat in background to finish the task
+        sleepUntilThreadFinish();
+
+        // check mailer configuration and email parameters, but don't send the email
+        verify(emailServiceMock).sendEmail(argumentCaptorMailer.capture(), argumentCaptorEmail.capture());
+        Session session = argumentCaptorMailer.getValue().getSession();
+        assertEquals("587", session.getProperty("mail.smtp.port"));
+        assertEquals("smtp", session.getProperty("mail.transport.protocol"));
+        assertEquals("smtp.gmail.com", session.getProperty("mail.smtp.host"));
+        assertEquals("test@mailaccount.com", session.getProperty("mail.smtp.username"));
+
+        assertEquals("Email body", argumentCaptorEmail.getValue().getText());
+        assertEquals("Email subject", argumentCaptorEmail.getValue().getSubject());
+
+        assertEquals("test@mailaccount.com", argumentCaptorEmail.getValue().getFromRecipient().getName());
+        assertEquals("test@mailaccount.com", argumentCaptorEmail.getValue().getFromRecipient().getAddress());
+
+        assertEquals("test1@mailaccount.com;test2@mailaccount.com", argumentCaptorEmail.getValue().getRecipients().get(0).getName());
+        assertEquals("test1@mailaccount.com", argumentCaptorEmail.getValue().getRecipients().get(0).getAddress());
+        assertEquals("test2@mailaccount.com", argumentCaptorEmail.getValue().getRecipients().get(1).getAddress());
+
+        assertEquals(2, argumentCaptorEmail.getValue().getRecipients().size());
+    }
+
+
+    @Test
+    public void testSendEmailSimpleDebug() throws Exception {
+        MultivaluedMap<String,Object> headers = new MultivaluedHashMap<>();
+        headers.add("Authorization", AuthorizationUtils.serialize(getDefaultAuthenticatedWho()));
+
+        when(emailServiceMock.createEmailSimple(any(), any())).thenCallRealMethod();
+
+        Response responseMsg = target("/actions/email-simple").request()
+                .headers(headers)
+                .post(getServerRequestFromFile("SendEmailController/simple-debug/request-send-simple-email.json"));
+
+        //check the response is right
+        assertJsonSame(
+                getJsonFormatFileContent("SendEmailController/simple-debug/response-send-simple-email.json"),
+                getJsonFormatResponse(responseMsg)
+        );
+
+        ArgumentCaptor<Email> argumentCaptorEmail = ArgumentCaptor.forClass(Email.class);
+        ArgumentCaptor<Mailer> argumentCaptorMailer = ArgumentCaptor.forClass(Mailer.class);
+
+        // before verify the call to send email, we DO NOT need to give some time to the threat in background to finish the task
+
+        // check mailer configuration and email parameters, but don't send the email
+        verify(emailServiceMock).sendEmail(argumentCaptorMailer.capture(), argumentCaptorEmail.capture());
+        Session session = argumentCaptorMailer.getValue().getSession();
+        assertEquals("587", session.getProperty("mail.smtp.port"));
+        assertEquals("smtp", session.getProperty("mail.transport.protocol"));
+        assertEquals("smtp.gmail.com", session.getProperty("mail.smtp.host"));
+        assertEquals("test@mailaccount.com", session.getProperty("mail.smtp.username"));
+
+        assertEquals("Email body", argumentCaptorEmail.getValue().getText());
+        assertEquals("Email subject", argumentCaptorEmail.getValue().getSubject());
+
+        assertEquals("test@mailaccount.com", argumentCaptorEmail.getValue().getFromRecipient().getName());
+        assertEquals("test@mailaccount.com", argumentCaptorEmail.getValue().getFromRecipient().getAddress());
+
+        assertEquals("test1@mailaccount.com;test2@mailaccount.com", argumentCaptorEmail.getValue().getRecipients().get(0).getName());
+        assertEquals("test1@mailaccount.com", argumentCaptorEmail.getValue().getRecipients().get(0).getAddress());
+        assertEquals("test2@mailaccount.com", argumentCaptorEmail.getValue().getRecipients().get(1).getAddress());
+
+        assertEquals(2, argumentCaptorEmail.getValue().getRecipients().size());
     }
 }
